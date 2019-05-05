4d5caa5d0451f5caa268d9baa9c7f0a5e56ff60a
Kavan Mannapperuma <kforkavan@live.com>
2019-05-05 10:53:59 +1000

    Add new components to seperate out the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes : Routes = [
+  { path: '', component: MeComponent},
+  { path: 'me', component: MeComponent},
+  { path: 'projects', component: MyProjectsComponent},
+  { path: 'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    MyProjectsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
